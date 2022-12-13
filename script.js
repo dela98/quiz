5e549493e40898fe686b73c3c33f0a36e82a1cc2
dela <dkagbodza@st.ug.edu.gh>
2022-12-13 16:10:17 +0000

    added javascript codes

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+//variable to hold selected answer
+let ans = ['Accra', 'Tovalds'];
 
+let check = function(){
+
+ let svall = document.querySelector("input[name=capital]:checked").value;
+
+ if(svall ==ans[0])
+ alert('correct');
+ else{
+  alert('wrong');
+  answers.innerHTML = 'Accra';
+ }
+
+}
